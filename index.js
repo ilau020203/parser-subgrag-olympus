8fd79ded12c152543ecb67d4360402b5d0bac42f
ilau020203 <antip19052016@gmail.com>
2021-10-13 11:25:40 +0300

    new example

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import {getManageByDay} from './manage/manageGetInformationForDays.js'
 import {getMintRewardsByDays} from './mint/mintGetInformationForDays.js'
 import {getMintRewardsByHours,getMintRewardsBy4Hours} from './mint/mintGetInformationForHours.js'
 import {getMintRewardsByMinutes} from './mint/mintGetInformationForMinutes.js'
-
+import {getBalanceForDay} from './controller_balance/controllerBalance.js'
 
 
 
@@ -19,38 +19,42 @@ import fs from 'fs'
 
 async function main() {
     //exapmle getting parser array from graphql 
+    // let data = JSON.stringify(await getBalanceForDay());
+    // fs.writeFileSync('resultBalanceDay.json', data);
+
+
     let data = JSON.stringify(await getTotalReserveByMinut());
     fs.writeFileSync('resultMinute.json', data);
-    data = JSON.stringify(await getTotalReserveByHour());
+    data = JSON.stringify(await getTotalReserveByHour(1633392000,1633996800));
     fs.writeFileSync('resultHour.json', data);
-    data = JSON.stringify(await getTotalReserveByDay());
+    data = JSON.stringify(await getTotalReserveByDay(1633392000,1633996800));
     fs.writeFileSync('resultDay.json', data);
-    data = JSON.stringify(await getTotalReserveBy4Hour());
+    data = JSON.stringify(await getTotalReserveBy4Hour(1633392000,1633996800));
     fs.writeFileSync('result4Hour.json', data);
-     data = JSON.stringify(await getDepositByMinut());
+     data = JSON.stringify(await getDepositByMinut(1633392000,1633996800));
     fs.writeFileSync('resultMinuteDeposit.json', data);
-     data = JSON.stringify(await getDepositByHour());
-    fs.writeFileSync('resultHourDeposit.json', data);
-     data = JSON.stringify(await getDepositByDay());
-    fs.writeFileSync('resultDayDeposit.json', data);
-    data = JSON.stringify(await getDepositBy4Hour());
-    fs.writeFileSync('result4HourDeposit.json', data);
-    data = JSON.stringify(await getManageByMinut());
-    fs.writeFileSync('resultMinuteManage.json', data);
-     data = JSON.stringify(await getManageByHour());
-    fs.writeFileSync('resultHourManage.json', data);
-     data = JSON.stringify(await getManageByDay());
-    fs.writeFileSync('resultDayManage.json', data);
-    data = JSON.stringify(await getManageBy4Hour());
-    fs.writeFileSync('result4HourManage.json', data);
-    data = JSON.stringify(await getMintRewardsByMinutes());
-    fs.writeFileSync('resultMintMinute.json', data);
-    data = JSON.stringify(await getMintRewardsByHours());
-    fs.writeFileSync('resultMintHour.json', data);
-    data = JSON.stringify(await getMintRewardsByDays());
-    fs.writeFileSync('resultMintDay.json', data);
-    data = JSON.stringify(await getMintRewardsBy4Hours());
-    fs.writeFileSync('resultMint4Hour.json', data);
+    //  data = JSON.stringify(await getDepositByHour(1633392000,1633996800));
+    // fs.writeFileSync('resultHourDeposit.json', data);
+    //  data = JSON.stringify(await getDepositByDay());
+    // fs.writeFileSync('resultDayDeposit.json', data);
+    // data = JSON.stringify(await getDepositBy4Hour());
+    // fs.writeFileSync('result4HourDeposit.json', data);
+    // data = JSON.stringify(await getManageByMinut());
+    // fs.writeFileSync('resultMinuteManage.json', data);
+    //  data = JSON.stringify(await getManageByHour());
+    // fs.writeFileSync('resultHourManage.json', data);
+    //  data = JSON.stringify(await getManageByDay());
+    // fs.writeFileSync('resultDayManage.json', data);
+    // data = JSON.stringify(await getManageBy4Hour());
+    // fs.writeFileSync('result4HourManage.json', data);
+    // data = JSON.stringify(await getMintRewardsByMinutes());
+    // fs.writeFileSync('resultMintMinute.json', data);
+    // data = JSON.stringify(await getMintRewardsByHours());
+    // fs.writeFileSync('resultMintHour.json', data);
+    // data = JSON.stringify(await getMintRewardsByDays());
+    // fs.writeFileSync('resultMintDay.json', data);
+    // data = JSON.stringify(await getMintRewardsBy4Hours());
+    // fs.writeFileSync('resultMint4Hour.json', data);
 }
 
 main();
