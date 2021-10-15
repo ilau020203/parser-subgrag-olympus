2626553fb1aa12ab59bed3da6fccbee5478549d9
ilau020203 <antip19052016@gmail.com>
2021-10-15 16:37:02 +0300

    add weth

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ import {getUniswapV2BalanceByDay} from './token_balance/uniswap_v2_balance/unisw
 import {getUniswapV2BalanceByHour,getUniswapV2BalanceBy4Hour} from './token_balance/uniswap_v2_balance/uniswapv2BalanceForHours.js'
 import {getUniswapV2BalanceByMinut} from './token_balance/uniswap_v2_balance/uniswapv2BalanceForMinutes.js'
 
+import {getWETHBalanceByDay} from './token_balance/weth_balance/wethBalanceForDays.js'
+import {getWETHBalanceByHour,getWETHBalanceBy4Hour} from './token_balance/weth_balance/wethBalanceForHours.js'
+import {getWETHBalanceByMinut} from './token_balance/weth_balance/wethBalanceForMinutes.js'
 
 
 
@@ -42,7 +45,7 @@ async function main() {
     // fs.writeFileSync('resultBalanceDay.json', data);
 
 
-    let data = JSON.stringify(await getTotalReserveByMinut(1633392000));
+    let data = JSON.stringify(await getTotalReserveByMinut());
     // fs.writeFileSync('resultMinute.json', data);
     // data = JSON.stringify(await getTotalReserveByHour(1633392000,1633996800));
     // fs.writeFileSync('resultHour.json', data);
@@ -54,17 +57,17 @@ async function main() {
     // fs.writeFileSync('resultMinuteDeposit.json', data);
     //  data = JSON.stringify(await getDepositByHour(1633392000,1633996800));
     // fs.writeFileSync('resultHourDeposit.json', data);
-    //  data = JSON.stringify(await getDepositByDay(1633392000,1633996800));
+    //  data = JSON.stringify(await getDepositByDay());
     // fs.writeFileSync('resultDayDeposit.json', data);
-    // data = JSON.stringify(await getDepositBy4Hour(1633392000));
+    // data = JSON.stringify(await getDepositBy4Hour());
     // fs.writeFileSync('result4HourDeposit.json', data);
-    // data = JSON.stringify(await getManageByMinut(1633392000));
+    // data = JSON.stringify(await getManageByMinut());
     // fs.writeFileSync('resultMinuteManage.json', data);
-    //  data = JSON.stringify(await getManageByHour(1633392000));
+    //  data = JSON.stringify(await getManageByHour());
     // fs.writeFileSync('resultHourManage.json', data);
-    //  data = JSON.stringify(await getManageByDay(1633392000));
+    //  data = JSON.stringify(await getManageByDay());
     // fs.writeFileSync('resultDayManage.json', data);
-    // data = JSON.stringify(await getManageBy4Hour(1633392000));
+    // data = JSON.stringify(await getManageBy4Hour());
     // fs.writeFileSync('result4HourManage.json', data);
     // data = JSON.stringify(await getMintRewardsByMinutes(1633392000));
     // fs.writeFileSync('resultMintMinute.json', data);
@@ -74,54 +77,62 @@ async function main() {
     // fs.writeFileSync('resultMintDay.json', data);
     // data = JSON.stringify(await getMintRewardsBy4Hours(1633392000));
     // fs.writeFileSync('resultMint4Hour.json', data);
-    data = JSON.stringify(await getDAIBalanceByMinut(1633392000));
-    fs.writeFileSync('resultDAIMinute.json', data);
-    data = JSON.stringify(await getDAIBalanceByHour(1633392000));
-    fs.writeFileSync('resultDAIHour.json', data);
-    data = JSON.stringify(await getDAIBalanceByDay(1633392000));
-    fs.writeFileSync('resultDAIDay.json', data);
-    data = JSON.stringify(await getDAIBalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultDAI4Hour.json', data);
-    data = JSON.stringify(await getFRAXBalanceByMinut(1633392000));
-    fs.writeFileSync('resultFRAXMinute.json', data);
-    data = JSON.stringify(await getFRAXBalanceByHour(1633392000));
-    fs.writeFileSync('resultFRAXHour.json', data);
-    data = JSON.stringify(await getFRAXBalanceByDay(1633392000));
-    fs.writeFileSync('resultFRAXDay.json', data);
-    data = JSON.stringify(await getFRAXBalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultFRAX4Hour.json', data);
-    data = JSON.stringify(await getLUSDBalanceByMinut(1633392000));
-    fs.writeFileSync('resultLUSDMinute.json', data);
-    data = JSON.stringify(await getLUSDBalanceByHour(1633392000));
-    fs.writeFileSync('resultLUSDHour.json', data);
-    data = JSON.stringify(await getLUSDBalanceByDay(1633392000));
-    fs.writeFileSync('resultLUSDDay.json', data);
-    data = JSON.stringify(await getLUSDBalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultLUSD4Hour.json', data);
-    data = JSON.stringify(await getlpOhmDaiBalanceByMinut(1633392000));
-    fs.writeFileSync('resultlpOhmDaiMinute.json', data);
-    data = JSON.stringify(await getlpOhmDaiBalanceByHour(1633392000));
-    fs.writeFileSync('resultlpOhmDaiHour.json', data);
-    data = JSON.stringify(await getlpOhmDaiBalanceByDay(1633392000));
-    fs.writeFileSync('resultlpOhmDaiDay.json', data);
-    data = JSON.stringify(await getlpOhmDaiBalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultlpOhmDai4Hour.json', data);
-    data = JSON.stringify(await getlpOhmLusdBalanceByMinut(1633392000));
-    fs.writeFileSync('resultlpOhmLusdMinute.json', data);
-    data = JSON.stringify(await getlpOhmLusdBalanceByHour(1633392000));
-    fs.writeFileSync('resultlpOhmLusdHour.json', data);
-    data = JSON.stringify(await getlpOhmLusdBalanceByDay(1633392000));
-    fs.writeFileSync('resultlpOhmLusdDay.json', data);
-    data = JSON.stringify(await getlpOhmLusdBalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultlpOhmLusd4Hour.json', data);
-    data = JSON.stringify(await getUniswapV2BalanceByMinut(1633392000));
-    fs.writeFileSync('resultUniswapV2Minute.json', data);
-    data = JSON.stringify(await getUniswapV2BalanceByHour(1633392000));
-    fs.writeFileSync('resultUniswapV2Hour.json', data);
-    data = JSON.stringify(await getUniswapV2BalanceByDay(1633392000));
-    fs.writeFileSync('resultUniswapV2Day.json', data);
-    data = JSON.stringify(await getUniswapV2BalanceBy4Hour(1633392000));
-    fs.writeFileSync('resultUniswapV24Hour.json', data);
+    // data = JSON.stringify(await getDAIBalanceByMinut(1633392000));
+    // fs.writeFileSync('resultDAIMinute.json', data);
+    // data = JSON.stringify(await getDAIBalanceByHour(1633392000));
+    // fs.writeFileSync('resultDAIHour.json', data);
+    // data = JSON.stringify(await getDAIBalanceByDay(1633392000));
+    // fs.writeFileSync('resultDAIDay.json', data);
+    // data = JSON.stringify(await getDAIBalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultDAI4Hour.json', data);
+    // data = JSON.stringify(await getFRAXBalanceByMinut(1633392000));
+    // fs.writeFileSync('resultFRAXMinute.json', data);
+    // data = JSON.stringify(await getFRAXBalanceByHour(1633392000));
+    // fs.writeFileSync('resultFRAXHour.json', data);
+    // data = JSON.stringify(await getFRAXBalanceByDay(1633392000));
+    // fs.writeFileSync('resultFRAXDay.json', data);
+    // data = JSON.stringify(await getFRAXBalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultFRAX4Hour.json', data);
+    // data = JSON.stringify(await getLUSDBalanceByMinut(1633392000));
+    // fs.writeFileSync('resultLUSDMinute.json', data);
+    // data = JSON.stringify(await getLUSDBalanceByHour(1633392000));
+    // fs.writeFileSync('resultLUSDHour.json', data);
+    // data = JSON.stringify(await getLUSDBalanceByDay(1633392000));
+    // fs.writeFileSync('resultLUSDDay.json', data);
+    // data = JSON.stringify(await getLUSDBalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultLUSD4Hour.json', data);
+    // data = JSON.stringify(await getlpOhmDaiBalanceByMinut(1633392000));
+    // fs.writeFileSync('resultlpOhmDaiMinute.json', data);
+    // data = JSON.stringify(await getlpOhmDaiBalanceByHour(1633392000));
+    // fs.writeFileSync('resultlpOhmDaiHour.json', data);
+    // data = JSON.stringify(await getlpOhmDaiBalanceByDay(1633392000));
+    // fs.writeFileSync('resultlpOhmDaiDay.json', data);
+    // data = JSON.stringify(await getlpOhmDaiBalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultlpOhmDai4Hour.json', data);
+    // data = JSON.stringify(await getlpOhmLusdBalanceByMinut(1633392000));
+    // fs.writeFileSync('resultlpOhmLusdMinute.json', data);
+    // data = JSON.stringify(await getlpOhmLusdBalanceByHour(1633392000));
+    // fs.writeFileSync('resultlpOhmLusdHour.json', data);
+    // data = JSON.stringify(await getlpOhmLusdBalanceByDay(1633392000));
+    // fs.writeFileSync('resultlpOhmLusdDay.json', data);
+    // data = JSON.stringify(await getlpOhmLusdBalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultlpOhmLusd4Hour.json', data);
+    // data = JSON.stringify(await getUniswapV2BalanceByMinut(1633392000));
+    // fs.writeFileSync('resultUniswapV2Minute.json', data);
+    // data = JSON.stringify(await getUniswapV2BalanceByHour(1633392000));
+    // fs.writeFileSync('resultUniswapV2Hour.json', data);
+    // data = JSON.stringify(await getUniswapV2BalanceByDay(1633392000));
+    // fs.writeFileSync('resultUniswapV2Day.json', data);
+    // data = JSON.stringify(await getUniswapV2BalanceBy4Hour(1633392000));
+    // fs.writeFileSync('resultUniswapV24Hour.json', data);
+    data = JSON.stringify(await getWETHBalanceByMinut());
+    fs.writeFileSync('resultWETHMinute.json', data);
+    data = JSON.stringify(await getWETHBalanceByHour());
+    fs.writeFileSync('resultWETHHour.json', data);
+    data = JSON.stringify(await getWETHBalanceByDay());
+    fs.writeFileSync('resultWETHDay.json', data);
+    data = JSON.stringify(await getWETHBalanceBy4Hour());
+    fs.writeFileSync('resultWETH4Hour.json', data);
 }
 
 main();
---
+++ token_balance/weth_balance/wethBalanceForDays.js
@@ -0,0 +1,114 @@
+import axios from 'axios'
+import { tokenForBalance } from '../../config.js';
+import {getWholePeriodOfTime} from '../../utils/date.js'
+
+const day =60*60*24;
+const dayQuery =`
+{
+    balanceYears(first:1000 orderBy:timestamp where:{token:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"}){
+      day(first:366 orderBy:timestamp) { 
+        value
+        id
+        timestamp
+      }
+    }
+  }
+`
+
+
+export async function getWETHBalanceByDay(startTimestamp=0,endTimestamp=Date.now()/1000) {
+    try{
+        return fillBigArrayForDays(reformToBigArrayForDays( await getWETHBalanceByDaysFromGraph()),startTimestamp,endTimestamp);
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
+
+async function getWETHBalanceByDaysFromGraph(){
+    try{
+        const dayData = await axios({
+            url: `https://api.thegraph.com/subgraphs/id/${tokenForBalance}`,//QmRpuXnecL1xjHgUUMSBaeok9Ggkpdep9KJNMLJxSbDvxZ
+            method: 'post',
+            data: {
+              query: dayQuery
+            }
+          })
+        return dayData.data.data.balanceYears;
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
+/**
+ * struct from subgrph reform to array
+ * @param {} days struct from subgrph
+ * @returns 
+ */
+function reformToBigArrayForDays(days){
+    let out=[];
+    for(let i=0; i<days.length; i++){
+        for(let j=0; j<days[i].day.length; j++){         
+            out.push(days[i].day[j]);
+        }
+    }
+    return out;
+}
+/**
+ * fills the array and divides it into equal time intervals
+ * @param {*} bigArray  
+ * @returns 
+ */
+function fillBigArrayForDays(bigArray,startTimestamp,endTimestamp){
+    let j=0;
+    while(bigArray[j].timestamp<startTimestamp) j++;
+
+
+    let out = [];
+    for(let i=j==0?1:j;i<bigArray.length;i++){
+        let timestamp=getWholePeriodOfTime(parseInt(bigArray[i-1].timestamp),day)
+        let nextTimestamp=getWholePeriodOfTime(parseInt(bigArray[i].timestamp),day)
+        if (timestamp>endTimestamp) return out;
+        if(timestamp>=startTimestamp){
+            out.push({
+                value:bigArray[i-1].value,
+                timestamp:timestamp,
+                audited:bigArray[i-1].audited,
+            });
+        }
+        timestamp+=day;
+        if (timestamp>endTimestamp) return out;
+        while(timestamp<nextTimestamp){
+            if(timestamp>=startTimestamp){
+                out.push({
+                    value:bigArray[i-1].value,
+                    timestamp:timestamp,
+                    audited:false,
+                });
+            }
+
+            timestamp+=day;
+        if (timestamp>endTimestamp) return out;
+        }
+        
+    }
+    
+    out.push({
+        value:bigArray[bigArray.length-1].value,
+        timestamp:getWholePeriodOfTime(parseInt(bigArray[bigArray.length-1].timestamp),day),////?
+        audited:bigArray[bigArray.length-1].audited,
+    })
+    let timestamp =getWholePeriodOfTime(parseInt(bigArray[bigArray.length-1].timestamp),day);
+    timestamp+=day;
+    while(timestamp<=endTimestamp){
+        out.push({
+            value:bigArray[bigArray.length-1].value,
+            timestamp:timestamp,
+            audited:false,
+        });
+        timestamp+=day;
+    }
+    return out;
+}
